1b2db3ba42e0756e40670e1a045167b8e05c274c
Dmitriy Trunin <dmitriy.trunin@kodep.ru>
2020-11-17 21:31:24 +0300

    init eslint and prettier

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
+import express from "express";
 
-import express from 'express'
+const app = express();
+const port = 8000;
 
-const app = express()
-const port = 8000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
+  console.log(`Example app listening at http://localhost:${port}`);
+});
