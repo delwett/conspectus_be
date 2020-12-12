c1a28ad0c0896c772dcaf2dd7b8cab461d8115ee
Dmitriy Trunin <dmitriy.trunin@kodep.ru>
2020-12-12 21:02:01 +0300

    prevent deletion completed tasks

--- src/services/tasks/delete-task.ts
+++ src/services/tasks/delete-task.ts
@@ -2,6 +2,7 @@ import { getManager } from 'typeorm'
 import getTaskById from './get-task-by-id'
 import BoardsService from '@/services/boards'
 import ValidationError from '@/errors/validation-error'
+import { TaskStatus } from '@/entities/task'
 
 export default async function createTask(id: string): Promise<void> {
   const task = await getTaskById(id)
@@ -9,6 +10,7 @@ export default async function createTask(id: string): Promise<void> {
   const currentBoard = await BoardsService.getCurrentBoard()
 
   if (task.boardId !== currentBoard.id) throw new ValidationError('Actions with tasks in old boards are prohibited')
+  if (task.status === TaskStatus.Completed) throw new ValidationError('Deletion of completed task is not allowed')
 
   await getManager().remove(task)
 }
