f63304d67154e27dc9ddca5cd7241a89cbeb3198
Dmitriy Trunin <dmitriy.trunin@kodep.ru>
2020-12-12 21:32:59 +0300

    add host env

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export const Stage = process.env.STAGE ?? 'development'
 export const JwtSecret = process.env.JWT_SECRET ?? ''
-export const Port = process.env.PORT ?? 8000
+export const Port = Number(process.env.PORT) ?? 8000
 export const ApiPath = process.env.API_PATH ?? '/graphql'
 export const ApiPlaygroundPath = process.env.API_PLAYGROUND_PATH ?? '/playground'
+export const Host = process.env.HOST ?? 'localhost'
--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import { createConnection } from 'typeorm'
 import { json as jsonBodyParserMiddleware } from 'body-parser'
 import gqlPlayground from 'graphql-playground-middleware-express'
 import graphqlMiddleware from '@/api'
-import { Stage, Port, ApiPath, ApiPlaygroundPath } from '@/config'
+import { Stage, Port, ApiPath, ApiPlaygroundPath, Host } from '@/config'
 
 const IsDevelopment = Stage !== 'production'
 
@@ -19,8 +19,8 @@ createConnection()
 
     if (IsDevelopment) app.get(ApiPlaygroundPath, gqlPlayground({ endpoint: ApiPath }))
 
-    app.listen(Port)
+    app.listen(Port, Host)
 
-    console.log(`GraphQL server started at http://localhost:${Port}${ApiPath}`)
+    console.log(`GraphQL server started at http://${Host}:${Port}${ApiPath}`)
   })
   .catch(error => console.error('TypeORM connection error: ', error))
