3dba9e712698fc3ac312c40a2653504c097e0081
Dmitriy Trunin <dmitriy.trunin@kodep.ru>
2020-12-24 01:44:09 +0300

    update type in activeusers query

--- src/api/queries/get-users.ts
+++ src/api/queries/get-users.ts
@@ -1,11 +1,11 @@
-import { GraphQLFieldConfig, GraphQLList } from 'graphql'
+import { GraphQLFieldConfig, GraphQLList, GraphQLNonNull } from 'graphql'
 import { UserType } from '@/api/definitions'
 import type { Context } from '@/api/types'
 import NotAuthorizedError from '@/errors/not-authorized-error'
 import UsersService from '@/services/users'
 
 const getUsers: GraphQLFieldConfig<undefined, Context> = {
-  type: new GraphQLList(UserType),
+  type: new GraphQLList(GraphQLNonNull(UserType)),
   resolve: async (_, __, context) => {
     if (!context.currentUser) throw new NotAuthorizedError()
 
