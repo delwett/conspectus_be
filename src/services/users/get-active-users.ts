6448a3def3ac2d55bd6089a7300442fe7544b609
Dmitriy Trunin <dmitriy.trunin@kodep.ru>
2020-12-23 22:53:41 +0300

    update active users order

--- src/services/users/get-active-users.ts
+++ src/services/users/get-active-users.ts
@@ -2,5 +2,5 @@ import { getManager } from 'typeorm'
 import { User } from '@/entities/user'
 
 export default async function getActiveUsers(): Promise<User[]> {
-  return getManager().find(User, { where: { deletedAt: null } })
+  return getManager().find(User, { where: { deletedAt: null }, order: { createdAt: 'ASC' } })
 }
